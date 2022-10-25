80a274ada0a53077e5decaacccfdec173c69a93a
dex_tver <ivan.sharapov.05@yandex.ru>
2022-10-25 16:47:18 +0300

    Initial commit

---
+++ test_pr/src/main/java/id/sharapov/test_pr/ApiController.java
@@ -0,0 +1,16 @@
+package id.sharapov.test_pr;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+@RestController
+public class ApiController {
+    @GetMapping("books")
+    public ResponseEntity<String> getText() {
+        return ResponseEntity.ok("Hello text");
+    }
+}
